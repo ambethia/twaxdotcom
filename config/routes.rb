2cc9d671b7d8617128b80aaee68df1ac4c4f78ea
Tyler Johnston <tylerjohnst@gmail.com>
2014-10-18 12:31:36 -0400

    Real home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   root to: 'visitors#index'
+  get '/real' => 'visitors#real'
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signin' => 'sessions#new', :as => :signin
   get '/signout' => 'sessions#destroy', :as => :signout
