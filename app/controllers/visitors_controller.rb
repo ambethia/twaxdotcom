212527f88af7dd8de93380fb6c2fa4a66e192ea2
Jason L Perry <jasper@ambethia.com>
2014-10-18 14:38:51 -0400

    Fsdf

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,7 +3,7 @@ class VisitorsController < ApplicationController
   end
 
   def placeholder
-    render layout: 'placeholder'
+    render layout: false
   end
 
   %i(real help help_left help_right).each do |action|
