f361868ce87b99ac3a175fdbbcd3ebf45d7c5df9
Jason L Perry <jasper@ambethia.com>
2014-10-18 13:25:40 -0400

    Post not get

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,5 @@ Rails.application.routes.draw do
   get '/auth/failure' => 'sessions#failure'
   get '/fax/receive' => 'webhooks#receive'
 
-  get '/mail' => 'webhooks#inbound_mail'
+  post '/mail' => 'webhooks#inbound_mail'
 end
