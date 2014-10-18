28f370caeb58d8e63ec91af66c1f40153df12faf
Jason L Perry <jasper@ambethia.com>
2014-10-18 13:49:57 -0400

    AGSSFA

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,5 @@ Rails.application.routes.draw do
   get '/auth/failure' => 'sessions#failure'
   get '/fax/receive' => 'webhooks#receive'
 
-  post '/mail' => 'webhooks#inbound_mail'
+  match '/mail' => 'webhooks#inbound_mail'
 end
